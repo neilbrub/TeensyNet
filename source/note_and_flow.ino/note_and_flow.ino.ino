a2cd662bdd46f2ed84d8fa5e3edaba7152656d17
Neil Brubacher <neil.brubacher@hotmail.com>
2019-06-30 22:27:04 -0400

    Adds flow sensor functionality & stub controller

--- source/note_and_flow.ino/note_and_flow.ino.ino
+++ source/note_and_flow.ino/note_and_flow.ino.ino
@@ -1,7 +1,7 @@
 unsigned long debounceDelay = 50;  // the debounce time; increase if the output flickers
 
 // **CHANGE ME!!**
-unsigned long flowCheckPeriod = 500;  // How long to accumulate flow sensor pulses before mapping to a volume
+unsigned long flowCheckPeriod = 50;  // How long to accumulate flow sensor pulses before mapping to a volume
 
 // Button input state
 int inputButtonValues[6] = {LOW, LOW, LOW, LOW, LOW, LOW};
@@ -11,11 +11,10 @@ unsigned long lastDebounceTimes[6] = {0, 0, 0, 0, 0, 0};  // the last time each 
 // Flow input state
 volatile unsigned long pulses;
 unsigned long last_pulseCheck;
-double last_volume;
 
 // Controller state
-char note_in_play;
-float output_volume;
+int note_playing;
+int current_volume;
 
 void setup() {
 
@@ -29,22 +28,29 @@ void setup() {
   pinMode(4, INPUT_PULLDOWN);
   pinMode(5, INPUT_PULLDOWN);
 
-  attachInterrupt(6, count_pulse, RISING)
+  attachInterrupt(6, count_pulse, RISING);
 
   pulses = 0;
   last_pulseCheck = 0;
-  last_volume = 0.0;
 
-  note_in_play = 'G';  // start open...
-  output_volume = 0.0;  // and off.
+  note_playing = 67;  // start open...
+  current_volume = 0;  // and off.
 }
 
-void controller(char note_to_play) {
-  Serial.print("Controller to play ");
-  Serial.println(note_to_play);
+// New note to play or 'no note change' signal passed, flow state assumed to always be correct
+void controller(int note_to_play) {
+  if (note_to_play != 0) {
+    Serial.print("Controller to play ");
+    Serial.print(note_to_play);
+    Serial.print(" at ");
+    Serial.println(current_volume);
+  } else {
+    Serial.print("Setting volume to: ");
+    Serial.println(current_volume);
+  }
 }
 
-char mapInputsToNote() {
+int mapInputsToNote() {
   // Setup some shorthands
   int val0 = inputButtonValues[0];
   int val1 = inputButtonValues[1];
@@ -53,23 +59,23 @@ char mapInputsToNote() {
   int val4 = inputButtonValues[4];
   int val5 = inputButtonValues[5];
 
-  // Invalid symbol '0'
-  char note = '0';
+  // Invalid symbol -1
+  int note = -1;
 
   // Here we go! Top to bottom for now, in two groups.
   if (!val3 && !val4 && !val5) {
     // Top group only
-    if (!val0 && !val1 && !val2) note = 'G';
-    else if (!val0 && val1 && !val2) note = 'F';  // actually want F#
-    else if (val0 && !val1 && !val2) note = 'E';
-    else if (val0 && val1 && !val2) note = 'D';
-    else if (val0 && val1 && val2) note = 'C';
+    if (!val0 && !val1 && !val2) note = 67;  // G
+    else if (!val0 && val1 && !val2) note = 66;  // F#
+    else if (val0 && !val1 && !val2) note = 64;  // E
+    else if (val0 && val1 && !val2) note = 62;  // D
+    else if (val0 && val1 && val2) note = 60;  // Middle C
   } else if (val0 && val1 && val2) {
-    // Bottom group (given top)
-    if (!val3 && val4 && !val5) note = 'B';
-    // Bb - val3 &&!val4 && !val5
-    else if (val3 && val4 && !val5) note = 'A';
-    else if (val3 && val4 && val5) note = 'G';
+    // Bottom group (given top all pressed)
+    if (!val3 && val4 && !val5) note = 59;  // B
+    else if (val3 &&!val4 && !val5) note = 58;  // Bb
+    else if (val3 && val4 && !val5) note = 57;  // A
+    else if (val3 && val4 && val5) note = 55;  // G
   }
 
   return note;
@@ -77,8 +83,11 @@ char mapInputsToNote() {
 
 void loop() {
 
+  // These occasionally need to persist - remember to reset them at EoL!
+  static bool buttonsChanged = false;
+  static bool flowChanged = false;
+
   /****** BUTTON DETECT ******/
-  bool inputStateChanged = false;
   for (int i = 0; i < 6; i++) {
     int reading = digitalRead(i);
     
@@ -94,7 +103,7 @@ void loop() {
       // if the button state has changed:
       if (reading != inputButtonValues[i]) {
         inputButtonValues[i] = reading;
-        inputStateChanged = true;
+        buttonsChanged = true;
       }
     }
     
@@ -103,22 +112,51 @@ void loop() {
 
   /****** AIRFLOW DETECT ******/
   if (millis() - last_pulseCheck >= flowCheckPeriod) {
-    // map number of pulses to a volume
-    // compare calculated value to last_volume; if changed:
-    //  - save the new volume to output_volume state
-    //  - invoke the controller to update the note being played
+    // Map # pulses to a volume (0 -> 127)
 
+    /**
+     * Experimental results:
+     *  blowing normally yielded ~150 pulses over 1 second
+     *  blowing hard yielded ~290 over 1 second
+     * 
+     */
+
+    unsigned int off_cap = (20 * flowCheckPeriod / 1000);
+    unsigned int low_cap= (120 * flowCheckPeriod / 1000);
+    unsigned int med_cap = (240 * flowCheckPeriod / 1000);
+    
+    int vol;
+    if (pulses <= off_cap) {
+      vol = 0;
+    } else if (pulses < low_cap) {
+      vol = 40;
+    } else if (pulses < med_cap) {
+      vol = 65;
+    } else {
+      vol = 100;
+    }
+
+    if (vol != current_volume) {
+      flowChanged = true;
+      current_volume = vol;
+    }
+    
     // debug:
-    Serial.print("Pulses: ");
-    Serial.println(pulses);
+//     Serial.print("Pulses: ");
+//     Serial.println(pulses);
     
     pulses = 0;
     last_pulseCheck = millis();
   }
 
-  if (inputStateChanged) {
+  if (buttonsChanged) {
     char note = mapInputsToNote();
     controller(note);
+    buttonsChanged = false;
+  } else if (flowChanged) {
+    // Send 'no note change' signal - controller uses note_playing from state
+    controller(0);
+    flowChanged = false;
   }
 }
 
