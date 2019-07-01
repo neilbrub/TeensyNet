07e03256eb700bdd330f558ad99ebb8d7343870c
asilverfox <aaronsilver27@gmail.com>
2019-06-30 21:52:11 -0400

    Changed logic flow, added invalid note mapping

--- source/controller/controller.ino
+++ source/controller/controller.ino
@@ -1,61 +1,63 @@
 int vol = 0;
-int note_in_play = 0;
-int channel = 1;
+int note_playing = 0;
 
 void setup(){
 }
 
+// To test controller function
+// Cycles through a few notes at different volumes
 void loop(){
-    vol = 100;
-    controller(1,40);
-    delay(1000);
-    vol = 25;
-    controller(0,40);
-    delay(1000);
-    vol = 15;
-    controller(0,40);
-    delay(1000);
-    vol = 120;
-    controller(0,40);
-    delay(1000);
-    vol = 100;
-    controller(1,60);
-    delay(1000);
-    vol = 25;
-    controller(0,60);
-    delay(1000);
-    vol = 15;
-    controller(0,60);
-    delay(1000);
-    vol = 120;
-    controller(0,60);
-    delay(1000);
+  vol = 100;
+  controller(67);
+  delay(1000);
+  vol = 100;
+  controller(55);
+  delay(1000);
+  vol = 25;
+  controller(40);
+  delay(1000);
+  vol = 0;
+  controller(40);
+  delay(1000);
+  vol = 120;
+  controller(40);
+  delay(1000);
+  vol = 100;
+  controller(60);
+  delay(1000);
+  vol = 25;
+  controller(60);
+  delay(1000);
+  vol = 15;
+  controller(60);
+  delay(1000);
+  vol = 120;
+  controller(0);
+  delay(1000);
+  controller(-1);
+  delay(1000);
 }
 
-void controller(int action_id, int note_to_play) {
-    if (action_id == 0) {
-        if (vol <= 0) {
-            usbMIDI.sendNoteOff(note_in_play, vol, channel);
-            note_in_play = 0;
-        }
-        else if (note_in_play) {
-            usbMIDI.sendNoteOff(note_in_play, vol, channel);
-            usbMIDI.sendNoteOn(note_in_play, vol, channel);
-        }
+void controller(int note_to_play) {
+  static int channel = 1;
+  if (vol <= 0) {
+    usbMIDI.sendNoteOff(note_playing, vol, channel);
+    note_playing = note_to_play;
+  }
+  else{
+    if (note_to_play == -1){
+      usbMIDI.sendNoteOff(note_playing, vol, channel);
+      usbMIDI.sendNoteOn(85, vol, channel);
+      note_playing = 85;
     }
-    else if (action_id == 1) {
-        if (vol <= 0) {
-            usbMIDI.sendNoteOff(note_in_play, vol, channel);
-            note_in_play = 0;
-        }
-        else if (note_in_play != note_to_play) {
-            usbMIDI.sendNoteOff(note_in_play, vol, channel);
-            usbMIDI.sendNoteOn(note_to_play, vol, channel);
-            note_in_play = note_to_play;
-        }
-        else {
-            usbMIDI.sendNoteOff(note_in_play, vol, channel);
-            usbMIDI.sendNoteOn(note_in_play, vol, channel);
-        }
+    else if (note_to_play == 0) {
+      usbMIDI.sendNoteOff(note_playing, vol, channel);
+      usbMIDI.sendNoteOn(note_playing, vol, channel);
     }
+    else{
+      usbMIDI.sendNoteOff(note_playing, vol, channel);
+      usbMIDI.sendNoteOn(note_to_play, vol, channel);
+      note_playing = note_to_play; 
+    }
+  }
 }
