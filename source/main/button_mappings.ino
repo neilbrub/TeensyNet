9e06b7528622cdb80af4994746c061669eeb3b40
asilverfox <aaronsilver27@gmail.com>
2019-07-23 19:53:48 -0400

    Add octave buttons functionality

--- source/main/button_mappings.ino
+++ source/main/button_mappings.ino
@@ -10,19 +10,24 @@ int mapInputsToNote() {
   // Invalid symbol -1  
   int note = -1;
 
-  if (!val1 && !val2 && !val3 && !val4 && !val5 && !val6) note = 70; //Bb4
-  else if (val1 && !val2 && !val3 && !val4 && !val5 && !val6) note = 69; //A4
-  else if (!val1 && val2 && !val3 && val4 && !val5 && !val6) note = 68;
-  else if (val1 && val2 && !val3 && !val4 && !val5 && !val6) note = 67; //G4
-  else if (val1 && !val2 && !val3 && val4 && !val5 && !val6) note = 66;
-  else if (val1 && val2 && !val3 && val4 && !val5 && !val6) note = 65; //F4
-  else if (!val1 && val2 && !val3 && !val4 && val5 && !val6) note = 64; //E4
-  else if (!val1 && !val2 && !val3 && !val4 && val5 && !val6) note = 63;
-  else if (val1 && !val2 && !val3 && !val4 && val5 && !val6) note = 62; //D4
-  else if (!val1 && !val2 && !val3 && val4 && val5 && !val6) note = 61;
-  else if (val1 && val2 && !val3 && !val4 && val5 && !val6) note = 60; //C4
-  else if (val1 && !val2 && !val3 && val4 && val5 && !val6) note = 59;
-  else if (val1 && val2 && !val3 && val4 && val5 && !val6) note = 58; //Bb3
+  if (!val1 && !val2 && !val4 && !val5) note = 70; //Bb4
+  else if (val1 && !val2 && !val4 && !val5) note = 69; //A4
+  else if (!val1 && val2 && val4 && !val5) note = 68;
+  else if (val1 && val2 && !val4 && !val5) note = 67; //G4
+  else if (val1 && !val2 && val4 && !val5) note = 66;
+  else if (val1 && val2 && val4 && !val5) note = 65; //F4
+  else if (!val1 && val2 && !val4 && val5) note = 64; //E4
+  else if (!val1 && !val2 && !val4 && val5) note = 63;
+  else if (val1 && !val2 && !val4 && val5) note = 62; //D4
+  else if (!val1 && !val2 && val4 && val5) note = 61;
+  else if (val1 && val2 && !val4 && val5) note = 60; //C4
+  else if (val1 && !val2 && val4 && val5) note = 59;
+  else if (val1 && val2 && val4 && val5) note = 58; //Bb3
+
+  if (val3) 
+    note += 12;
+  if (val6)
+    note -= 12;
 
   return note;
 }
